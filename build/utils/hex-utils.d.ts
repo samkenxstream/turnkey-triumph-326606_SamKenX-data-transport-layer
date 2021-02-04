a2975cf11dc84a04bb39f0ed7db254ccbda54cb4
Kelvin Fichter <kelvinfichter@gmail.com>
2021-02-03 19:06:09 -0500

    Remove erroneous build folder

--- build/utils/hex-utils.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference types="node" />
-export declare const fromHexString: (buf: Buffer | string) => Buffer;
-export declare const toHexString: (buf: Buffer | string | number | null) => string;
--- build/utils/index.d.ts
+++
@@ -1,4 +0,0 @@
-export * from './common';
-export * from './constants';
-export * from './contracts';
-export * from './hex-utils';
