7404627b72e1376f46d636243906f6fa97f6ccd1
Kelvin Fichter <kelvinfichter@gmail.com>
2021-02-19 13:16:27 -0500

    Remove orDefault

--- src/services/run.ts
+++ src/services/run.ts
@@ -5,49 +5,43 @@ import Config from 'bcfg' // TODO: Add some types for bcfg if we get the chance.
 /* Imports: Internal */
 import { L1DataTransportService } from './main/service'
 
-// TODO: Temporary way to assign defaults. We need to merge service-base#1 so that `null` is
-// treated as an empty value and assigns defaults properly.
-const orDefault = (val: any, defaultVal: any): any => {
-  if (val === undefined || val === null) {
-    return defaultVal
-  } else {
-    return val
-  }
+interface Bcfg {
+  load: (options: { env?: boolean; argv?: boolean }) => void
+  str: (name: string, defaultValue?: string) => string
+  uint: (name: string, defaultValue?: number) => number
+  bool: (name: string, defaultValue?: boolean) => boolean
 }
 
 ;(async () => {
   try {
     dotenv.config()
 
-    const config = new Config('data-transport-layer')
+    const config: Bcfg = new Config('data-transport-layer')
     config.load({
       env: true,
       argv: true,
     })
 
     const service = new L1DataTransportService({
-      db: orDefault(config.str('dbPath'), './db'),
-      port: orDefault(config.uint('serverPort'), '7878'),
-      hostname: orDefault(config.str('serverHostname'), 'localhost'),
-      confirmations: orDefault(config.uint('confirmations'), 12),
+      db: config.str('dbPath', './db'),
+      port: config.uint('serverPort', 7878),
+      hostname: config.str('serverHostname', 'localhost'),
+      confirmations: config.uint('confirmations', 12),
       l1RpcProvider: config.str('l1RpcEndpoint'),
       addressManager: config.str('addressManager'),
-      pollingInterval: orDefault(config.uint('pollingInterval'), 5000),
-      logsPerPollingInterval: orDefault(
-        config.uint('logsPerPollingInterval'),
-        2000
-      ),
-      dangerouslyCatchAllErrors: orDefault(
-        config.bool('dangerouslyCatchAllErrors'),
+      pollingInterval: config.uint('pollingInterval', 5000),
+      logsPerPollingInterval: config.uint('logsPerPollingInterval', 2000),
+      dangerouslyCatchAllErrors: config.bool(
+        'dangerouslyCatchAllErrors',
         false
       ),
       l2RpcProvider: config.str('l2RpcEndpoint'),
-      l2ChainId: orDefault(config.uint('l2ChainId'), 69),
-      syncFromL1: orDefault(config.bool('syncFromL1'), true),
-      syncFromL2: orDefault(config.bool('syncFromL2'), false),
-      showUnconfirmedTransactions: orDefault(config.bool('syncFromL2'), false),
-      transactionsPerPollingInterval: orDefault(
-        config.uint('transactionsPerPollingInterval'),
+      l2ChainId: config.uint('l2ChainId', 69),
+      syncFromL1: config.bool('syncFromL1', true),
+      syncFromL2: config.bool('syncFromL2', false),
+      showUnconfirmedTransactions: config.bool('syncFromL2', false),
+      transactionsPerPollingInterval: config.uint(
+        'transactionsPerPollingInterval',
         1000
       ),
     })
